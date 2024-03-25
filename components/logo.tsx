aa66c3300fcdef47e2bfebedcb25cd61588d9791
Stefan Ristic <casino@ib.dev>
2024-03-25 09:34:52 +0100

    Removes console logs

--- components/logo.tsx
+++ components/logo.tsx
@@ -4,18 +4,11 @@ import { useConfig } from 'nextra-theme-docs'
 export const Logo = () => {
   const { darkMode, ...others } = useConfig()
 
-  console.log(others)
-  console.log(darkMode)
-
   return (
     <>
       <img
         style={{ width: '40px', marginRight: '10px', borderRadius: '100%' }}
-        src={
-          darkMode
-            ? 'https://fastloyal-dev-image-storage.s3.eu-west-1.amazonaws.com//Group+1321315353+(2).png'
-            : 'https://fastloyal-dev-image-storage.s3.eu-west-1.amazonaws.com//Group+1321315379.png'
-        }
+        src={'https://fastloyal-dev-image-storage.s3.eu-west-1.amazonaws.com//Group+1321315353+(2).png'}
       />
       <h2 style={{ fontWeight: 'bold' }}>UNBX</h2>
     </>
